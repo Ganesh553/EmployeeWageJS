b43c86ab0e708447eb29fd3bfed05bf2f0135cc1
Ganesh553 <72643633+Ganesh553@users.noreply.github.com>
2021-01-17 21:07:54 +0530

    Update EmployeeWage.js

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,43 +1,49 @@
 class EmployeeWage{
+
+    checkEmployee(){
+        let Employee = Math.floor(Math.random() * 2);
+        if(Employee==0)
+            console.log("Employee is Present")
+        else{
+            console.log("Employee is Absent")
+        }
+        
+    }
+
      employeeDailyWage(){
-        var Full_Time = 1;
-        var Part_Time = 2;
+        var Full_Time = 0;
+        var Part_Time = 1;
         var Emp_Rate_Per_Hr = 20;
         var emp_Hr = 0;
-        var Absent=0;
+        var Total_Emp_Hr=0;
         var NUMBER_OF_WORKING_DAYS=20;
         var Total_Employee_Wage=0;
         var day=0;
+        var Total_Working_Day =20;
         var DailyWage = new Map()
-        for (var Day=1;Day <= NUMBER_OF_WORKING_DAYS; Day++){
-            let Employee = Math.floor(Math.random() * 3);
+        while (day<=Total_Working_Day && Total_Emp_Hr<=100 ){
+            let Employee = Math.floor(Math.random() * 2);
             switch(Employee){
                 case Full_Time:
                     emp_Hr=8;
-                    DailyWage[day++] = emp_Hr;
+                    DailyWage[day++] = emp_Hr*Emp_Rate_Per_Hr;
+                    Total_Emp_Hr = Total_Emp_Hr+emp_Hr;
                     break;
                 case Part_Time:
-                    emp_Hr=16;
-                    DailyWage[day++] = emp_Hr;
-                    break;
-                case Absent:
-                    console.log("Employee Is Absent");
+                    emp_Hr=4;
+                    DailyWage[day++] = emp_Hr*Emp_Rate_Per_Hr;
+                    Total_Emp_Hr = Total_Emp_Hr+emp_Hr;
                     break;
             }
             var Employee_Wage = Emp_Rate_Per_Hr * emp_Hr;
             Total_Employee_Wage= Total_Employee_Wage + Employee_Wage;           
         }
-        DailyWage["Employee Daily Wage"] =Employee_Wage
         DailyWage["Total_Employee_Wage"] =Total_Employee_Wage
         console.log(DailyWage)
-        console.log("Employee Daily Wage: ",Employee_Wage)
-        console.log("Total Wage of Employee For 20 Days: ",Total_Employee_Wage)
-    }
-    WorkingHrs(day, hours){
-        return day*hours;
+        console.log("Total Working Hours is: ",Total_Emp_Hr)
+        console.log("Total Wage of Employee : ",Total_Employee_Wage)
     }
 }
 let ref = new EmployeeWage()
+ref.checkEmployee()
 ref.employeeDailyWage();
-var total_Working_Hours=ref.WorkingHrs(20,8)
-console.log("Total number of Working Hours is: ",total_Working_Hours)
